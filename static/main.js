5fc0366e90f6d493eb5ae97d84886e84b48f3958
Daniil325 <potekhindaniill@mail.ru>
2023-11-10 00:53:12 +0700

    fix for safari

--- static/main.js
+++ static/main.js
@@ -1,3 +1,13 @@
+let header = document.querySelector('.header');
+let width = window.screen.width;
+
+if (width >= 900){
+    header.innerHTML += "<img class='header-image' src='/media/Шапка%20пк%20(1)-min.png' alt=''>"
+}
+else{
+    header.innerHTML += "<img class='header-image' src='/media/Шапка%20мобил%201-min.png' alt=''>"
+}
+
 let pat = document.querySelector('.footer_pattern')
 let pat_w = pat.offsetWidth;
 
@@ -8,4 +18,4 @@ while (w - 65 > 0){
     w -= 65
 }
 
-console.log(pat)
+console.log(pat);
