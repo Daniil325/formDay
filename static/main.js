1f2f4aad89085a7831202e32b6c984ba849f718f
Daniil325 <potekhindaniill@mail.ru>
2023-11-09 23:47:28 +0700

    fix design

--- static/main.js
+++ static/main.js
@@ -3,9 +3,9 @@ let pat_w = pat.offsetWidth;
 
 let w = pat.offsetWidth - 10;
 
-while (w - 46 > 0){
-    pat.innerHTML += "<img class=\"pattern_image\" src=\"/media/паттерн.png\" alt=\"\">";
-    w -= 46
+while (w - 65 > 0){
+    pat.innerHTML += "<img class='pattern_image' src='/media/паттерн.png'>";
+    w -= 65
 }
 
 console.log(pat)
