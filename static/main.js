8b730fc93099a6ca431e91a8c7bc59de1c29c576
Daniil325 <potekhindaniill@mail.ru>
2023-11-13 18:42:40 +0700

    add images and picture tag

--- static/main.js
+++ static/main.js
@@ -1,13 +1,3 @@
-let header = document.querySelector('.header');
-let width = window.screen.width;
-
-if (width >= 900){
-    header.innerHTML += "<img class='header-image' src='/media/Шапка%20пк%20(1)-min.png' alt=''>"
-}
-else{
-    header.innerHTML += "<img class='header-image' src='/media/Шапка%20мобил%201-min.png' alt=''>"
-}
-
 let pat = document.querySelector('.footer_pattern')
 let pat_w = pat.offsetWidth;
 
